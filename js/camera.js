264825783a0583d162d8a27ae2509085ae09fd41
Takashi Yagita <yagita.takashi@gmail.com>
2014-06-14 11:04:12 +0900

    change js

---
+++ js/camera.js
@@ -0,0 +1,19 @@
+if (!navigator.getUserMedia && !navigator.webkitGetUserMedia) {
+  alert('webkit系ブラウザでないか、もしくはgetUserMediaがサポートされていません');
+}
+
+function getMedia() {
+  navigator.webkitGetUserMedia(
+    {video: true},
+    function(localMediaStream) {
+      var myVideo  = document.getElementById('myVideo');
+      myVideo.src = window.URL.createObjectURL(localMediaStream);
+      myVideo.play();
+    },
+    function(err) {
+      alert('カメラから映像を取得することができませんでした。');  
+      console.log(err);
+    }
+  );
+}
+
